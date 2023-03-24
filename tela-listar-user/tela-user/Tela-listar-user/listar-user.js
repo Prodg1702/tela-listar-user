a4eb599005892c041608b836f3f660df04add8fc
Prodg1702 <106077622+Prodg1702@users.noreply.github.com>
2023-03-24 08:55:10 -0300

    Add files via upload

---
+++ tela-listar-user/tela-user/Tela-listar-user/listar-user.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const redBox = document.querySelector('.box.red');
+    redBox.style.display = 'none';
+  });
+  
+  const buttons = document.querySelectorAll('.button-change');
+  const blueBox = document.querySelector('.table_docente');
+  const redBox = document.querySelector('.table_admin');
+  
+  buttons.forEach((button) => {
+    button.addEventListener('click', function() {
+      buttons.forEach((button) => {
+        button.classList.remove('active');
+      });
+      this.classList.add('active');
+      const color = this.getAttribute('data-color');
+      if (color === 'orange') {
+        blueBox.style.display = 'block';
+        redBox.style.display = 'none';
+      } else if (color === 'gray') {
+        blueBox.style.display = 'none';
+        redBox.style.display = 'block';
+      }
+    });
+  });
+  
+
+// ========================================================================
+
+// modais
+
+var modalUser = document.querySelector(".modal-cad-user");
+var open_modalUser = document.querySelector(".open_modalUser");
+var closeButtonUser = document.querySelector(".close-button-user");
+var changeOpacidad = document.querySelector(".navega-color");
+
+
+// essa function é pura gambiarra, mas sem tempo irmão
+function backgroundChange(){
+  changeOpacidad.classList.remove("navega-change")
+}
+
+function toggleModalUser() {
+    modalUser.classList.toggle("show-modal");
+    changeOpacidad.classList.add("navega-change")
+
+}
+
+function windowOnClick(event) {
+    if (event.target === modalUser) {
+        toggleModalUser();
+        changeOpacidad.classList.remove("navega-change")
+    }
+}
+
+open_modalUser.addEventListener("click", toggleModalUser);
+closeButtonUser.addEventListener("click", toggleModalUser);
+closeButtonUser.addEventListener("click", backgroundChange);
+window.addEventListener("click", windowOnClick);
+
+
+//Modal 2 -- editar
+
+var modalEdit = document.querySelector(".modal-edit-user");
+var open_modal_edit = document.querySelector(".open_modal-Edit");
+var closeButton_edit = document.querySelector(".close-button-edit");
+
+function toggleModalEdit() {
+    modalEdit.classList.toggle("show-modal-edit");
+    changeOpacidad.classList.add("navega-change")
+}
+
+function windowOnClick2(event) {
+    if (event.target === modalEdit) {
+        toggleModalEdit();
+        changeOpacidad.classList.remove("navega-change")
+    }
+}
+
+open_modal_edit.addEventListener("click", toggleModalEdit);
+closeButton_edit.addEventListener("click", toggleModalEdit);
+closeButton_edit.addEventListener("click", backgroundChange)
+window.addEventListener("click", windowOnClick2);
+
+
+
+
+
